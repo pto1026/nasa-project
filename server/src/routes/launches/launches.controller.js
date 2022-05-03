7b9b181cafed49e8c8a35ea870c1e11334e882f6
Patrick Sparks <pto1026@gmail.com>
2022-05-03 05:13:09 -0600

    added validation on the post /launch endpoint

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,22 +1,35 @@
-const {
-    getAllLaunches,
-    addNewLaunch
-} = require("../../models/launches.model");
+const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
 
-function httpGetAllLaunches (req, res) {
-    return res.status(200).json(getAllLaunches);
+function httpGetAllLaunches(req, res) {
+  return res.status(200).json(getAllLaunches);
 }
 
 function httpAddNewLaunch(req, res) {
-    const launch = req.body;
+  const launch = req.body;
 
-    launch.launchDate = new Date(launch.launchDate);
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.destination
+  ) {
+      return res.status(400).json({
+          error: "Missing required launch data",
+      })
+  }
 
-    addNewLaunch(launch);
-    return res.status(201).json(launch);
+  launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+      return res.status(400).json({
+          error: "Invalid launch date",
+      })
+  }
+
+  addNewLaunch(launch);
+  return res.status(201).json(launch);
 }
 
 module.exports = {
-    httpGetAllLaunches,
-    httpAddNewLaunch
-}
+  httpGetAllLaunches,
+  httpAddNewLaunch,
+};
